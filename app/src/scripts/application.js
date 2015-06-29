cacbb7ffdf35f9337380c8e82a0d191ec64afa05
leandro <leandro@localhost.localdomain>
2015-06-28 21:31:12 -0300

    Carrousel de Destaques

--- app/src/scripts/application.js
+++ app/src/scripts/application.js
@@ -1,3 +1,15 @@
-function function_name(argument) {
-  // body...
-}
+$('#destaques > .carousel .item').each(function(){
+  var next = $(this).next();
+  console.log(next);
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
