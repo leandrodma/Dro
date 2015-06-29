e73430ffaf29f9c0ade2572e9618f5676069f674
Leandro Martins <leandrodmax@gmail.com>
2015-06-29 09:45:54 -0300

    Carousel images highlights

--- app/src/scripts/application.js
+++ app/src/scripts/application.js
@@ -1,15 +1,19 @@
 $('#destaques > .carousel .item').each(function(){
+
   var next = $(this).next();
-  console.log(next);
+
   if (!next.length) {
     next = $(this).siblings(':first');
   }
+
   next.children(':first-child').clone().appendTo($(this));
 
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    next.children(':first-child').clone().appendTo($(this));
   }
-  else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
+
 });
